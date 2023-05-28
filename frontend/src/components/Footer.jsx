2fe5bf8be348f1c248bb1b9715968d982a66105d
kkeroo <jasko10.kerec@gmail.com>
2023-05-28 10:08:47 +0200

    Fix footer.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="footer-dark mt-5" style={{borderTop: '2px solid #DDD'}}>
+        <div className="footer-dark mt-5 fixed-bottom" style={{borderTop: '2px solid #DDD'}}>
         <footer>
             <div className="container mt-2">
                 <div className="row text-center">
